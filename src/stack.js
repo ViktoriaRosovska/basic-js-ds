d512ccc25a8f21f8bc3c8e57ad252a7cfe2b2ab4
Viktoria Rosovska <torimeister@gmail.com>
2025-01-05 20:08:39 +0200

    feat: solve task stack

--- src/stack.js
+++ src/stack.js
@@ -13,19 +13,25 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  stackArr = [];
+  push( element ) {
+    return this.stackArr.push(element);
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.stackArr.length) {
+      return this.stackArr.pop();
+    } else {
+      return undefined;
+    }
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if (this.stackArr.length) {
+      return this.stackArr[this.stackArr.length - 1];
+    } else {
+      return undefined;
+    }
   }
 }
 
